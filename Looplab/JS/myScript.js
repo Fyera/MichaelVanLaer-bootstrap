b064c022452105430bd042435789364f6fd162ea
MichaëlVL <michaelvanlaer@gmail.com>
2020-05-11 17:28:19 +0200

    LoopLAB

--- Looplab/JS/myScript.js
+++ Looplab/JS/myScript.js
@@ -1,4 +1,23 @@
-console.log('Dit werkt');
+// console.log('Dit werkt');
 
 //Get the current year for the copyright
 $('#year').text(new Date().getFullYear());
+
+// Init Scrollspy
+$('body').scrollspy({ target: '#main-nav' });
+
+//Smooth Scrolling
+$('#main-nav a').on('click', function(event) {
+    if (this.hash !== "") {
+        event.preventDefault();
+
+        const hash = this.hash;
+        console.log(hash);
+
+        $('html,body').animate({
+            scrollTop: $(hash).offset().top
+        }, 800, function() {
+            window.location.hash = hash;
+        });
+    }
+});
